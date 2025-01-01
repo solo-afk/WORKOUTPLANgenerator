298eba5a91041322a90dd12265da1ca8c6d8a56e
solo-afk <solomoncoverdale@gmail.com>
2025-01-01 15:16:08 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,6 @@ import streamlit as st
 import os
 import google.generativeai as ggi
 
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
 
  # Page title
     
